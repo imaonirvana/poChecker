6dafe495eded013e324262fcf64fd18d70cdb916
imaonirvana <dfun801@gmail.com>
2024-01-26 13:26:42 +0200

    Now duplicates are written to a separate file

--- error_writer.py
+++ error_writer.py
@@ -2,13 +2,20 @@ import os
 
 class ErrorWriter:
     @staticmethod
-    def write_duplicate_error(output_file, file_path, line_num, duplicate_string):
+    def write_duplicate_error(output_file, file_path, line_num, duplicate_string, duplicate_file):
         with open(output_file, 'a', encoding='utf-8') as output:
             output.write("===========================================================================\n")
             output.write(f"Error in {os.path.basename(file_path)}, line {line_num}:\n")
             output.write(f"Description: Duplicate string found\n")
             output.write(f"Original: '{duplicate_string}'\n")
             output.write("===========================================================================\n\n")
+
+        with open(duplicate_file, 'a', encoding='utf-8') as duplicate:
+            duplicate.write("===========================================================================\n")
+            duplicate.write(f"Error in {os.path.basename(file_path)}, line {line_num}:\n")
+            duplicate.write(f"Description: Duplicate string found\n")
+            duplicate.write(f"Original: '{duplicate_string}'\n")
+            duplicate.write("===========================================================================\n\n")
 
     @staticmethod
     def write_error(output_file, file_path, line_num, description, original, translated=None):
