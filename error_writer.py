c153a5e77bf11f7dd5e13007d7cea90fc1c6c320
imaonirvana <dfun801@gmail.com>
2024-01-27 15:50:58 +0200

    Add private static variables for output files

--- error_writer.py
+++ error_writer.py
@@ -1,25 +1,28 @@
 import os
 
 class ErrorWriter:
-    @staticmethod
-    def write_duplicate_error(output_file, file_path, line_num, duplicate_string, duplicate_file):
-        with open(output_file, 'a', encoding='utf-8') as output:
+    _general_output = "all_errors.txt"
+    _duplicate_output = "duplicates.txt"
+
+    @classmethod
+    def write_duplicate_error(cls, file_path, line_num, duplicate_string):
+        with open(cls._general_output, 'a', encoding='utf-8') as output:
             output.write("===========================================================================\n")
             output.write(f"Error in {os.path.basename(file_path)}, line {line_num}:\n")
             output.write(f"Description: Duplicate string found\n")
             output.write(f"Original: '{duplicate_string}'\n")
             output.write("===========================================================================\n\n")
 
-        with open(duplicate_file, 'a', encoding='utf-8') as duplicate:
+        with open(cls._duplicate_output, 'a', encoding='utf-8') as duplicate:
             duplicate.write("===========================================================================\n")
             duplicate.write(f"Error in {os.path.basename(file_path)}, line {line_num}:\n")
             duplicate.write(f"Description: Duplicate string found\n")
             duplicate.write(f"Original: '{duplicate_string}'\n")
             duplicate.write("===========================================================================\n\n")
 
-    @staticmethod
-    def write_error(output_file, file_path, line_num, description, original, translated=None):
-        with open(output_file, 'a', encoding='utf-8') as output:
+    @classmethod
+    def write_error(cls, file_path, line_num, description, original, translated=None):
+        with open(cls._general_output, 'a', encoding='utf-8') as output:
             output.write("===========================================================================\n")
             output.write(f"Error in {os.path.basename(file_path)}, line {line_num}:\n")
             output.write(f"Description: {description}\n")
