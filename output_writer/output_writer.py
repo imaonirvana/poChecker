7188ab7b907c0cf23bb67d37a8a58bde309bd796
imaonirvana <dfun801@gmail.com>
2024-01-30 02:19:02 +0200

    removed test part for Windows

--- output_writer/output_writer.py
+++ output_writer/output_writer.py
@@ -2,7 +2,7 @@ class OutputWriter:
     _file_path: str
 
     def __init__(self, file_path: str):
-        self._file_path = file_path.replace('\\', '/')
+        self._file_path = file_path
 
     def write_to_line(self, content: str, line_num: int):
         with open(self._file_path, 'r', encoding="utf-8") as file:
